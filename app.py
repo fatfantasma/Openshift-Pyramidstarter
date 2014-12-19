3080bbef005ac799d121968770cdd0a677f054fa
mrfatboy <mrfatboy@sbcglobal.com>
2014-12-18 16:06:56 -0800

    refactor code

--- app.py
+++ app.py
@@ -6,29 +6,23 @@ from pyramid.paster import get_appsettings
 
 if __name__ == '__main__':
     here = os.path.dirname(os.path.abspath(__file__))
-    config = os.path.join(here, 'production.ini')
 
-    app = get_app(config, 'main')
-    settings = get_appsettings(config, 'main')
-
-    if 'OPENSHIFT_PYTHON_IP' in os.environ:
+    if 'OPENSHIFT_APP_NAME' in os.environ:                                          #are we on OPENSHIFT?
         ip = os.environ['OPENSHIFT_PYTHON_IP']
         port = int(os.environ['OPENSHIFT_PYTHON_PORT'])
+        config = os.path.join(here, 'production.ini')
     else:
-        ip = '0.0.0.0'                                          #localhost
+        ip = '0.0.0.0'                                                              #localhost
         port = 6543
+        config = os.path.join(here, 'development.ini')
 
+    app = get_app(config, 'main')                                                   #find 'main' method in __init__.py.  That is our wsgi app
+    settings = get_appsettings(config, 'main')                                      #don't really need this but is an example on how to get settings from the '.ini' files
 
 # Waitress (remember to include the waitress server in "install_requires" in the setup.py)
-#     from waitress import serve
-#     print("Starting Waitress Server on http://{0}:{1}".format(ip, port))
-#     serve(app, host=ip, port=port, threads=50)
-
-#Simple Server
-    from wsgiref.simple_server import make_server
-    print("Starting Simple Server on http://{0}:{1}".format(ip, port))
-    server = make_server(ip, port, app)
-    server.serve_forever()
+    from waitress import serve
+    print("Starting Waitress.")
+    serve(app, host=ip, port=port, threads=50)
 
 # Cherrypy server (remember to include the cherrypy server in "install_requires" in the setup.py)
 #     from cherrypy import wsgiserver
@@ -36,6 +30,12 @@ if __name__ == '__main__':
 #     server = wsgiserver.CherryPyWSGIServer((ip, port), app, server_name='Server')
 #     server.start()
 
+
+#Simple Server
+    # from wsgiref.simple_server import make_server
+    # print("Starting Simple Server on http://{0}:{1}".format(ip, port))
+    # server = make_server(ip, port, app)
+    # server.serve_forever()
 
 #Running 'production.ini' method manually.  I find this method the least compatible with Openshift since you can't
 #easily start/stop/restart your app with the 'rhc' commands. Mabye somebody can suggest a better way :)
@@ -45,9 +45,10 @@ if __name__ == '__main__':
 # You also will have to set up another custom action hook so rhc app-restart, stop works.
 # See Openshifts Origin User's Guide  ( I have not tried this yet)
 
-#        print('Running pserve production.ini')
-#        os.system("pserve production.ini &")
+#Method #1
+    # print('Running pserve production.ini')
+    # os.system("pserve production.ini &")
 
-#this method works also.
+#Method #2
     #import subprocess
     #subprocess.Popen(['pserve', 'production.ini &'])
--- pyramidstarter/__init__.py
+++ pyramidstarter/__init__.py
@@ -6,7 +6,6 @@ def main(global_config, **settings):
     """
     config = Configurator(settings=settings)
     config.include('pyramid_chameleon')
-    config.include('pyramid_chameleon')
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
     config.scan()

0e2b3e04b81fb690e4634045e06a5ae8bfa795d8
mrfatboy <mrfatboy@sbcglobal.com>
2014-12-24 11:38:32 -0800

    touch so git sees

--- wsgi.py
+++ wsgi.py
@@ -3,6 +3,7 @@ import os
 import sys
 from pyramid.paster import get_app
 
+
 here = os.path.dirname(os.path.abspath(__file__))
 pyramid_working_folder = os.path.basename(here).lower()
 sys.path.insert(0, os.path.join(here, pyramid_working_folder))
